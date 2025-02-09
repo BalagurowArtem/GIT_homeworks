193bb5a15749127617beab4e6d3e480131431cce
BalagurowArtem <balagurow2017@gmail.com>
2025-02-09 15:52:04 +0900

    классы jsonfile, citiesserializer

--- hw8/hw8.py
+++ hw8/hw8.py
@@ -13,3 +13,46 @@ class City:
     longitude: float
     is_used: bool = False
 
+class JsonFile:
+    def __init__(self, filename: str):
+        self.filename = filename
+        
+    def read_data(self)-> List[Dict[str, Any]]:
+        with open(self.filename, 'r', encoding='utf-8') as f:
+            return json.load(f)
+        
+    def write_data(self, data: List[Dict[str, Any]]):
+
+        with open(self.filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+class CitiesSerializer:
+    def __init__(self, city_data: List[Dict[str, Any]]):
+    
+        self.cities: List[City] = []
+        self._serialize_cities(city_data)
+
+    def _serialize_cities(self, city_data: List[Dict[str, Any]]):
+
+        for city in city_data:
+           
+            self.cities.append(
+                City(
+                    name=city['name'],
+
+                    population=city['population'],
+
+                    subject=city['subject'],
+
+                    district=city['district'],
+
+                    latitude=float(city['coords']['lat']),
+
+                    longitude=float(city['coords']['lon']),
+
+                    
+                )
+            )
+    
+    def get_all_cities(self) -> List[City]:
+        return self.cities
